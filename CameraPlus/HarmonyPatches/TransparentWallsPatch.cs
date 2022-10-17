ef48ca82f6545879cf67611e032b78aa71b7d32f
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-10-17 20:45:56 +0900

    なんかビートリーダーのリプレイでバグる

--- CameraPlus/HarmonyPatches/TransparentWallsPatch.cs
+++ CameraPlus/HarmonyPatches/TransparentWallsPatch.cs
@@ -9,14 +9,18 @@ namespace CameraPlus.HarmonyPatches
         public static int WallLayerMask = 25;
         static void Postfix(Transform ____obstacleCore, ref ParametricBoxFakeGlowController ____obstacleFakeGlow)
         {
-            Camera.main.cullingMask |= (1 << TransparentWallsPatch.WallLayerMask);//Enables HMD bits because layer 25 is masked by default
-            if (____obstacleCore != null)
-            {
-                ____obstacleCore.gameObject.layer = WallLayerMask;
+            try {
+                Camera.main.cullingMask |= (1 << TransparentWallsPatch.WallLayerMask);//Enables HMD bits because layer 25 is masked by default
+                if (____obstacleCore != null) {
+                    ____obstacleCore.gameObject.layer = WallLayerMask;
 
-                if (____obstacleFakeGlow.enabled)
-                    ____obstacleCore.GetChild(0).gameObject.layer = WallLayerMask;
+                    if (____obstacleFakeGlow != null && ____obstacleFakeGlow.enabled)
+                        ____obstacleCore.GetChild(0).gameObject.layer = WallLayerMask;
+                }
             }
+            catch {
+            }
+            
         }
     }
 }
