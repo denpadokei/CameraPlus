3163cb9c9dfb82de69dabe91a2cd668279573240
denpadokei <jsoldenpa@yahoo.co.jp>
2024-05-15 19:54:04 +0900

    try catch

--- CameraPlus/HarmonyPatches/CustomPreviewBeatmapLevelPatch.cs
+++ CameraPlus/HarmonyPatches/CustomPreviewBeatmapLevelPatch.cs
@@ -1,42 +1,51 @@
-﻿using System.Collections.Generic;
+﻿using HarmonyLib;
 using System.IO;
-using HarmonyLib;
-using IPA.Utilities;
-using CameraPlus.HarmonyPatches;
 
 namespace CameraPlus.HarmonyPatches
 {
     [HarmonyPatch(typeof(LevelSelectionNavigationController), nameof(LevelSelectionNavigationController.HandleLevelCollectionNavigationControllerDidChangeLevelDetailContent))]
     internal static class SongScriptBeatmapPatch
     {
-        private static string _keyPrefix = "custom_level_";
-        private static string _customLevelRoot = CustomLevelPathHelper.customLevelsDirectoryPath;
+        private static readonly string _keyPrefix = "custom_level_";
+        private static readonly string _customLevelRoot = CustomLevelPathHelper.customLevelsDirectoryPath;
 
         private static string _latestSelectedLevelPath = string.Empty;
         public static string customLevelPath = string.Empty;
-        static void Postfix(LevelSelectionNavigationController __instance)
+
+        private static void Postfix(LevelSelectionNavigationController __instance)
         {
-            if (CustomLevelLoaderPatch.Instance._loadedBeatmapSaveData.ContainsKey(__instance.beatmapLevel.levelID))
+            try
             {
-                string currentLevelPath = CustomLevelLoaderPatch.Instance._loadedBeatmapSaveData[__instance.beatmapLevel.levelID].customLevelFolderInfo.folderPath;
-                if (currentLevelPath != _latestSelectedLevelPath)
+                if (CustomLevelLoaderPatch.Instance._loadedBeatmapSaveData.ContainsKey(__instance.beatmapLevel.levelID))
                 {
-                    _latestSelectedLevelPath = currentLevelPath;
+                    var currentLevelPath = CustomLevelLoaderPatch.Instance._loadedBeatmapSaveData[__instance.beatmapLevel.levelID].customLevelFolderInfo.folderPath;
+                    if (currentLevelPath != _latestSelectedLevelPath)
+                    {
+                        _latestSelectedLevelPath = currentLevelPath;
 #if DEBUG
                     Plugin.Log.Notice($"Selected CustomLevel Path :\n {currentLevelPath}");
 #endif
-                    if (File.Exists(Path.Combine(currentLevelPath, "SongScript.json")))
-                    {
-                        customLevelPath = Path.Combine(currentLevelPath, "SongScript.json");
-                        Plugin.Log.Notice($"Found SongScript path : \n{currentLevelPath}");
-                    }
-                    else
-                    {
-                        customLevelPath = string.Empty;
+                        if (File.Exists(Path.Combine(currentLevelPath, "SongScript.json")))
+                        {
+                            customLevelPath = Path.Combine(currentLevelPath, "SongScript.json");
+                            Plugin.Log.Notice($"Found SongScript path : \n{currentLevelPath}");
+                        }
+                        else
+                        {
+                            customLevelPath = string.Empty;
+                        }
                     }
                 }
-            }else
+                else
+                {
+                    customLevelPath = string.Empty;
+                }
+            }
+            catch (System.Exception e)
+            {
+                Plugin.Log.Error(e);
                 customLevelPath = string.Empty;
+            }
         }
     }
 }
